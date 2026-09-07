95569e7e4727af63a3174b01b0130a797778aea0
Your Name <akbarperdana49@gmail.com>
2026-09-07 14:53:57 +0700

    Remove build artifacts from repository

--- build/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/raylib/src
-# Build directory: /workspace/build/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
--- build/ui/CMakeFiles/hsr_ui.dir/src/widgets/Sidebar.cpp.o.d
+++
@@ -1,4 +0,0 @@
-ui/CMakeFiles/hsr_ui.dir/src/widgets/Sidebar.cpp.o: \
- /workspace/ui/src/widgets/Sidebar.cpp /usr/include/stdc-predef.h \
- /workspace/ui/src/widgets/Sidebar.h /workspace/raylib/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/12/include/stdarg.h
